7f70c6cc429fcdd25184819f0452e3be965c8bc1
Mridul Gupta <emgi@live.in>
2022-12-21 10:31:19 +0530

    added constraints and initial hamiltonian

--- solver.py
+++ solver.py
@@ -6,6 +6,7 @@ from findiff import FinDiff
 from scipy.sparse.linalg import inv, eigs
 from scipy.sparse import coo_array, eye, diags, kron
 import matplotlib.pyplot as plt
+import sys
 
 def expand(val: int, known):
     j = 0
@@ -24,7 +25,19 @@ def expand(val: int, known):
         val >>= 1
     return x
 
+def exchange(n, **kwargs):
+    return eye(n, **kwargs)[::-1]
 
+constraints = []
+# Row Constraints
+for i in [0,1,8,9,16,24]:
+    constraints.append((i, i+2, i+4, i+6))
+# Column Constraints
+for i in range(8):
+    constraints.append((i, i+8, i+16, i+24))
+# Block Constraints
+for i in [0,1,4,5,16,17,20,21]:
+    constraints.append((i, i+2, i+8, i+10))
 
 # Get input
 query = ''
@@ -60,3 +73,8 @@ for i, it in enumerate(exp_query):
     else:
         known_vals.append((i, int(it)))
      
+# Make Hamiltonians
+# Initial Hamiltonian
+g   =   10. # strength
+H0  =   g*exchange(1<<complexity, dtype = 'D', format='csr')
+# Constructing the final hamiltonian
