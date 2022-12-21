655e455562249b02e20947561462befcc0706d98
Mridul Gupta <emgi@live.in>
2022-12-21 17:43:50 +0530

    Completed simulater

--- solver.py
+++ solver.py
@@ -7,23 +7,46 @@ from scipy.sparse.linalg import inv, eigs
 from scipy.sparse import csr_matrix, eye, diags, kron
 import matplotlib.pyplot as plt
 import sys
+import gc
+from tqdm import tqdm
 
+fno = 0
+def plot(state, dt, it, t):
+    global fno, fig
+    fno+=1
+    plt.ylim((-14, 0.5))
+    plt.xlabel(r"$x_{10}$")
+    plt.ylabel(r"$\log(Prob(x))$")
+    plt.grid()
+    plt.plot(2*np.log(np.abs(state)))
+    plt.title(f"dt={dt:.3e} it={it:8d} t={t:6.2f}")
+    plt.savefig(f"Images/{fno:05d}.jpg", dpi=240, bbox_inches = 'tight')
+    plt.clf()
+    plt.close()
+ 
 def expand(val: int, known):
     j = 0
     x = 0
     for i, it in known:
         while (j<i):
-            x   <<= 1
-            x   +=  val&1
-            val >>  1
+            x = 2*x + val%2
+            val = val//2
             j+=1
-        x   <<= 1
-        x   +=  it
+        x = 2*x + it
+        j+=1
     while(val>0):
-        x   <<= 1
-        x   +=  val&1
-        val >>= 1
+        x = 2*x + val%2
+        val = val//2
     return x
+
+def sum_sx(N:int):
+    I = eye(2, format='csr')
+    sx = csr_matrix([[0,1],[1,0]], dtype = int)
+    Z = sx
+    for i in range(1, N):
+        Z = csr_matrix(kron(Z, I) + kron(eye(1<<i, format='csr'), sx))
+    return Z
+
 
 def exchange(n, **kwargs):
     return eye(n, **kwargs)[::-1]
@@ -48,31 +71,23 @@ for i in [0,1,4,5,16,17,20,21]:
     constraints.append((i, i+2, i+8, i+10))
 
 def cost(a,b,c,d):
-    global  known_vals_dict, complexity
-    print(a, b, c, d)
-    I = eye(1<<complexity)
-    unk = 0
+    global  known_vals_dict, complexity, I
     if a in known_vals_dict.keys():
         A = known_vals_dict[a]*I
     else:
         A = x_op(index[a], complexity)
-        unk+=1
     if b in known_vals_dict.keys():
         B = known_vals_dict[b]*I
     else:
         B = x_op(index[b], complexity)
-        unk+=1
     if c in known_vals_dict.keys():
         C = known_vals_dict[c]*I
     else:
         C = x_op(index[c], complexity)
-        unk+=1
     if d in known_vals_dict.keys():
         D = known_vals_dict[d]*I
     else:
         D = x_op(index[d], complexity)
-        unk+=1
-    print(unk)
     z = (A+B+C+D-2*I)
     return z*z
 
@@ -111,12 +126,65 @@ for i, it in enumerate(exp_query):
     else:
         known_vals.append((i, int(it)))
         known_vals_dict[i] = int(it)
+
+I = eye(1<<complexity)
+
 # Make Hamiltonians
 # Initial Hamiltonian
-g   =   10. # strength
-H0  =   g*exchange(1<<complexity, dtype = 'D', format='csr')
+g   =   2. # strength
+H0  =   g*sum_sx(complexity)
 # Constructing the final hamiltonian
-Hf = cost(*constraints[0])
+a,b,c,d = constraints[0]
+Hf = cost(a,b,c,d)
 for a,b,c,d in constraints[1:]:
     Hf += cost(a,b,c,d)
+# General time Hamiltonian
+def H(s):
+    return (1-s)*H0 + s*Hf
+
+def U(s, dt):
+    forward = (I-1.j*dt*H(s))
+    # backward = (I+0.5j*dt*H(s))
+    return forward
+
+# Simulate Evolution by Euler approximation
+
+# Initialize state
+pauli_x_ground = 1/np.sqrt(2)*np.matrix([1,-1], dtype='D').H
+state = pauli_x_ground
+for _ in range(complexity-1):
+    state = np.kron(state, pauli_x_ground)
     
+T = 100
+ts, dt = np.linspace(0, T, 10_000, retstep=True, dtype='d')
+
+for it in tqdm(range(len(ts))):
+    state = U(ts[it]/T, dt)*state
+    # Renormalize
+    state = state/np.sqrt((state.H*state))
+    if it%13 == 1: plot(state, dt, it, ts[it])
+
+def reencode(x):
+    xb = bin(x)[2:].zfill(32)
+    xb = list(xb)
+    ren = ''
+    while xb:
+        a = xb.pop(0)
+        b = xb.pop(0)
+        val = 2*int(a) + int(b) + 1
+        ren += str(val)
+    return ren
+
+# Sort Results
+sol_sort = []
+prob_dist = np.power(np.abs(state), 2)  
+avg_prob = np.average(prob_dist)
+Solutions = np.where(prob_dist>avg_prob)[0]
+for sol in Solutions:
+    sol_sort.append((sol, expand(sol, known_vals), reencode(expand(sol, known_vals)),prob_dist[sol][0,0]))
+
+sol_sort.sort(key = lambda x:x[-1], reverse=True)
+print(f"{0:6d} : {0:20d} : {bin(0)[2:].zfill(32)} : {query} : {0:.8f}")
+print("-"*100)
+for a,b,c,d in sol_sort:
+    print(f"{a:6d} : {b:20d} : {bin(b)[2:].zfill(32)} : {c} : {d:.8f}")
